a0bb058e364bbd073de6b0eda7b1e3a07c5fc2c0
varshaa1708 <gvarshaa20006@gmail.com>
2026-03-23 10:40:49 +0530

    Add UC1

---
+++ App/src/HotelBookingApp.java
@@ -0,0 +1,11 @@
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("\nWelcome to the Hotel Booking Management System");
+
+        // System Initialization Message
+        System.out.println("System initialized successfully.");
+    }
+}
